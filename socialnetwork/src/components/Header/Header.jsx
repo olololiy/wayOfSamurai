592016cc67b05b37dc2add4c57ff6b704803e870
Artemy <64956500+olololiy@users.noreply.github.com>
2022-01-30 17:03:33 +0300

    study grids

---
+++ socialnetwork/src/components/Header/Header.jsx
@@ -0,0 +1,12 @@
+import s from './Header.module.css';
+
+
+function Header() {
+    return (
+        <header className={s.header}>
+            <h1>My social network</h1>
+        </header>
+    )
+}
+
+export default Header;
---
+++ socialnetwork/src/components/Profile/Profile.jsx
@@ -0,0 +1,12 @@
+import s from './Profile.module.css'
+
+function Profile() {
+    return(
+        <div>
+            <h2>Something</h2>
+            <img src="https://www.baikal-olkhon.ru/pic/9768d2c3130abef1eec1e0b4e6b952e0/saraiskiy_leto.jpg"></img>
+        </div>
+    )
+}
+
+export default Profile;
